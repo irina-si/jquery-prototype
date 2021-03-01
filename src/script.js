1202cf4c91d82c491851be748b4e7a27c7d94c4c
unknown <sidorenko.irina.d@gmail.com>
2021-03-01 19:41:53 +0300

    addClassModule

--- src/script.js
+++ src/script.js
@@ -1,12 +1,10 @@
 // import modules
 import {addClassWithClassList} from './modules/addClassModule.js';
 import {addClass} from './modules/addClassModule.js';
-//import {removeClass} from './modules/removeClassModule.js';
 
 //  New methods of Element object
 Element.prototype.addClass = addClass;
 Element.prototype.addClassWithClassList = addClassWithClassList;
-//Element.prototype.removeClass = removeClass;
 
 // Testing
 const body = document.body;
@@ -18,7 +16,6 @@ let element3 = document.querySelector('#three');
 element1.addClass('red orange-text');
 element2.addClass(['red', 'orange-text'])
 element3.addClass(returnClass);
-//element1.removeClass(returnClass);;
 
 function returnClass() {
     const red = 'red';
